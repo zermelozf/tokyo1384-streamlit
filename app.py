049cd8c9d7bbcc57e46a8b5475bd01dad9218ab8
Arnaud Rachez <arnaud@linalgo.com>
2024-02-03 09:59:50 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+
+name = st.selectbox(
+    'Who is this?',
+    ('Christine', 'Ryan', 'JFK'))
+
+
+st.markdown(f"""# This is a {name}
+## This is a sub header
+This is text
+            
+1. one
+2. two
+3. three""")
+
+@st.cache
+def df():
+ return pd.DataFrame({
+        'first column': list(range(1, 11)),
+        'second column': np.random.randint(100, size=10)
+    })
+
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
+
+# and used to select the displayed lines
+h = df().head(line_count)
+h
